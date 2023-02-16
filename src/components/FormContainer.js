9f217c4926de3d978cef8a4782d5f434caef9ad6
minhneit <102277357+minhneit@users.noreply.github.com>
2023-02-16 13:47:22 +0700

    Update FormContainer.js

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -2,7 +2,7 @@ import { Container, Col, Row } from 'react-bootstrap';
 
 function FormContainer({ children }) {
     return (
-        <Container className="bg-success py-5">
+        <Container className="bg-success py-5 rounded-2">
             <Row className="justify-content-md-center row">
                 <Col sx={12} md={9}>
                     {children}
